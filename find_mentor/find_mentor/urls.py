72d883d3c0c1bceb853b6dfbd47b9be87a8edce2
theuppercaseguy <saadan06@gmail.com>
2022-06-15 22:47:49 +0500

    resolved some issues

--- find_mentor/find_mentor/urls.py
+++ find_mentor/find_mentor/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include("main_web.urls", namespace='main_web')),
+    path('main_web/',include("main_web.urls", namespace='main_web')),
     path('',include("django.contrib.auth.urls")),
 ]
